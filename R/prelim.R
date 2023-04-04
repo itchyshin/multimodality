d1140eeaea70bace76977855eb5d82b848b9c3fb
Shinichi Nakagawa <s.nakagawa@unsw.edu.au>
2023-04-05 07:02:30 +0900

    small test to see if I can push using ssh

--- R/prelim.R
+++ R/prelim.R
@@ -14,4 +14,15 @@ library(orchaRd)
 
 dat <- read.csv(here("data/dat_04_04_2023.csv"))
 
+# function for calculating variance
+Vd < function(d, n1, n2, design = c("within","between"), r = 0.5){}
+
+
+
+# filtering some crazy values
+dat %>% 
+
+
+
+
 
