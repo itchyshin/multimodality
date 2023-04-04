3255687a5089bee8ba325332ce2c75321edf93d5
Shinichi Nakagawa <s.nakagawa@unsw.edu.au>
2023-04-05 06:43:35 +0900

    initial commit

--- R/prelim.R
+++ R/prelim.R
@@ -1,1 +1,17 @@
 # some preliminary analysis to do
+
+# install
+
+#install.packages("pacman")
+#pacman::p_load(devtools, tidyverse, metafor, patchwork, R.rsp, emmeans)
+
+#devtools::install_github("daniel1noble/orchaRd", force = TRUE)
+
+library(tidyverse)
+library(here)
+library(lme4)
+library(orchaRd)
+
+dat <- read.csv(here("data/dat_04_04_2023.csv"))
+
+
